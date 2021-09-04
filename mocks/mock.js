6943bbec71067caecc0eecd83058902973319a9b
JeniferMorati <jenifermorati26@gmail.com>
2021-09-04 17:02:16 -0300

    feat(Mocks)
    Create mocks for products

---
+++ mocks/mock.js
@@ -0,0 +1,50 @@
+const products = [
+  {
+    "id": 1,
+    "title": " Tux Sudo",
+    "price": 4.22,
+    "image": "https://mlohrktvfr9b.i.optimole.com/scftOdE.BaVg~16a4d/w:247/h:247/q:90/https://www.nerdstickers.com.br/wp-content/uploads/2020/11/adesivo-00-TuxSudo-NS.png"
+  },
+  {
+    "id": 2,
+    "title": "Need More Space",
+    "price": 4.90,
+    "image": "https://mlohrktvfr9b.i.optimole.com/scftOdE.BaVg~16a4d/w:247/h:247/q:90/https://www.nerdstickers.com.br/wp-content/uploads/2020/11/adesivo-need-more-space-nerd-stickers.png"
+  },
+  {
+    "id": 3,
+    "title": "Fullstack dev",
+    "price": 5,
+    "image": "https://mlohrktvfr9b.i.optimole.com/scftOdE.BaVg~16a4d/w:247/h:247/q:90/https://www.nerdstickers.com.br/wp-content/uploads/2020/11/Adesivo-Fullstack-Dev-Nerd-Stickers.png"
+  },
+  {
+    "id": 5,
+    "title": "Front Ninja",
+    "price": 3.87,
+    "image": "https://mlohrktvfr9b.i.optimole.com/scftOdE.BaVg~16a4d/w:247/h:247/q:90/https://www.nerdstickers.com.br/wp-content/uploads/2020/11/Adesivo-Ninja-Nerd-Stickers.png"
+  },
+  {
+    "id": 6,
+    "title": "NASA",
+    "price": 4.32,
+    "image": "https://mlohrktvfr9b.i.optimole.com/scftOdE.BaVg~16a4d/w:247/h:247/q:90/https://www.nerdstickers.com.br/wp-content/uploads/2020/11/14395367561-1.jpeg"
+  },
+  {
+    "id": 4,
+    "title": "Octocat II",
+    "price": 3.90,
+    "image": "https://mlohrktvfr9b.i.optimole.com/scftOdE.BaVg~16a4d/w:247/h:247/q:90/https://www.nerdstickers.com.br/wp-content/uploads/2020/11/STK-227-Octocat-II-525X525cm-1.jpg"
+  },
+  {
+    "id": 8,
+    "title": " Extreme go horse",
+    "price": 4.32,
+    "image": "https://mlohrktvfr9b.i.optimole.com/scftOdE.BaVg~16a4d/w:600/h:600/q:90/https://www.nerdstickers.com.br/wp-content/uploads/2020/12/adesivo-extreme-go-horse-ns.png"
+  },
+  {
+    "id": 7,
+    "title": " Red Bug",
+    "price": 4.32,
+    "image": "https://mlohrktvfr9b.i.optimole.com/scftOdE.BaVg~16a4d/w:600/h:600/q:90/https://www.nerdstickers.com.br/wp-content/uploads/2020/11/adesivo-00-RedBug-NS.png"
+  }
+];
